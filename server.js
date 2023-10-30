07afc2601bfc97cecaf48a19157bb6cc1e85242d
priyanshujha1997 <priyanshu.jha@niveussolutions.com>
2023-10-30 16:03:37 +0530

    assignment second

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express()
+
+const port =  8000 
+
+app.use(express.json()) //enable json request body parsing
+
+//http://localhost:8000/api/info/:id?param=queryParamValue
+app.get('/api/info/:id', (req, res) => {
+    const { id } = req.params;
+    const queryParam = req.query.param;
+  
+    const info = {
+      pathParam: id,
+      queryParam: queryParam,
+    };
+  console.log(info)
+    res.json(info);
+  });
+
+  const dataArray = [];
+
+  app.post('/api/data', (req, res) => {
+    const { data } = req.body;
+  
+    if (data) {
+      dataArray.push(data);
+    return  res.json({ success: true, data, message: 'Data added successfully' });
+    } else {
+      res.status(400).json({ success: false, message: 'Invalid data' });
+    }
+  });
+    
+
+
+app.post('/api/data',(req, res) =>{
+    const {body}=req
+    res.json(body)
+})
+
+
+app.post('/post',(req, res)=>{
+    req.send(req.body)
+})
+
+app.listen(port,()=>{
+    console.log(`Server listen on port ${port}`)
+})
